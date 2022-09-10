5a5e70efda6c4b7c8ae6cab12ba24fe0976a0862
nishthat <nishtha.taktewale12@gmail.com>
2022-09-10 22:15:38 +0530

    added insertion sort algorithm

--- Sorting.c
+++ Sorting.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
 
+// Function to print an array of size n
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// INSERTION SORT
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
 int main(){
   int sort;
   int n;
@@ -12,6 +37,13 @@ int main(){
   for (int i = 0; i < n; i++)
   {
     scanf("%d", &arr[i]);
+  }
+  switch (sort) {
+    case 1:
+    printf("Sorted array using Insertion Sort: \n");
+    insertionSort(arr, n);
+    printArray(arr, n);
+    break;
   }
   return 0;
 }
