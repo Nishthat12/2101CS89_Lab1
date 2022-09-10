6b36be46bd92a4602835415c7b97ffb0d79f3552
nishthat <nishtha.taktewale12@gmail.com>
2022-09-10 20:17:35 +0530

    added the addition operation

---
+++ calculator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int op;
+  double a,b;
+  while(1){
+    printf("Enter the number of the operation \n1. Addition(+)\n2. Subtraction(-)\n3. Multiplication(*)\n4.Division(/)\nIf you want to exit press 0\n");
+    scanf("%d", &op);
+    if (op == 0)
+    {
+        exit(0);
+    }
+    printf("Enter the first and second numbers\n");
+    scanf("%lf %lf",&a,&b);
+    switch (op)
+    {
+    case 1:
+        printf("%.1lf + %.1lf = %.1lf\n", a, b, a + b);
+        break;
+    }
+  }
+}
