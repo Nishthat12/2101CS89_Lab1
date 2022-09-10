9bdb61ffe67c51a89939e90f8b65fb1db03bd2f0
nishthat <nishtha.taktewale12@gmail.com>
2022-09-10 22:26:54 +0530

    added merge sort algorithm

--- Sorting.c
+++ Sorting.c
@@ -63,6 +63,63 @@ void bubbleSort(int arr[], int n)
                 swap(&arr[j], &arr[j + 1]);
 }
 
+// MERGE SORT
+//merge the two halves
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements*/
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements*/
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        // sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
 int main(){
   int sort;
   int n;
@@ -92,6 +149,12 @@ int main(){
     case 3:
     printf("Sorted array using Bubble Sort: \n");
     bubbleSort(arr, n);
+    printArray(arr, n);
+    break;
+
+    case 4:
+    printf("Sorted array using Merge Sort: \n");
+    mergeSort(arr, 0, n - 1);
     printArray(arr, n);
     break;
   }
